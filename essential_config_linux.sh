d7dcb9873f75f37b410a8fd5d38ad88c4fcc5a5c
Joao Collier de Mendonca <joao.collierdemendonca@siemens-healthineers.com>
2020-06-02 12:11:49 +0200

    add giveme8 alias

--- essential_config_linux.sh
+++ essential_config_linux.sh
@@ -19,7 +19,8 @@ EOF
 
 cat << 'EOF' >> ~/.bash_aliases
 
-alias 7zinfected='7z a sample-`myts`-pwd-infected.zip -tzip -mem=AES256 -pinfected'
+alias 7zinfected='7z a sample-$(date '+%Y%m%d-%H%M%S%z')-pwd-infected.zip -tzip -mem=AES256 -pinfected'
+
 alias bashRemoveSpacesFromFilenames='for name in *\ *; do mv -v "$name" "${name// /}"; done'
 alias cO='curl -O'
 alias download='cO'
@@ -42,10 +43,14 @@ alias mktemp32='echo `cat /dev/urandom | base64 | tr -dc a-zA-Z0-9 | fold -w 32 
 alias mp='ps ajf'
 alias mydate='date +%Y%m%d'
 alias myts='date +%Y%m%d-%H%M%S%z'
+# alias myts="date '+%Y%m%d-%H%M%S%z'"
 
 alias sortipv4='sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n'
 alias squidtime='awk '\''{printf "%s", strftime("%F %T UTC", $1, 1);   $1=""; print $0}'\'''
 alias ts_to_date_linux='echo \`date "+%Y%m%d-%H%M%S%z" -ud @1415161718\`, where 1415161718 is your timestamp to convert'
+
+alias giveme8='head -c 1024 /dev/urandom | LC_ALL=C tr -cd "[:alpha:]" | fold -w 8'
+
 EOF
 
 
